4cce535c443cd77507699aae2198b23c3c9c8343
Joseph Herrera <tony.herrera90@gmail.com>
2020-11-13 15:11:06 -0800

    Validated JPA/Hiberanate config

--- JPAGarrison/src/main/java/com/skilldistillery/garrison/entities/Veteran.java
+++ JPAGarrison/src/main/java/com/skilldistillery/garrison/entities/Veteran.java
@@ -1,5 +1,72 @@
 package com.skilldistillery.garrison.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Veteran {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public Veteran(int id, String firstName) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+	}
+
+	public Veteran() {
+		super();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Veteran other = (Veteran) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Veteran [id=" + id + ", firstName=" + firstName + "]";
+	}
 
 }
--- JPAGarrison/src/test/java/com/skilldistillery/garrison/entities/GarrisonTest.java
+++ JPAGarrison/src/test/java/com/skilldistillery/garrison/entities/GarrisonTest.java
@@ -1,34 +1,58 @@
 package com.skilldistillery.garrison.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class GarrisonTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager  em;
+	private Veteran vet;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("GarrisonPU");
+		
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		vet = em.find(Veteran.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		
+		em.close();
+		vet = null;
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("Veteran entity mapping")
+	void test1() {
+		
+		assertNotNull(vet);
+		assertEquals("Tony", vet.getFirstName());
+		
+		
+		
 	}
 
 }
