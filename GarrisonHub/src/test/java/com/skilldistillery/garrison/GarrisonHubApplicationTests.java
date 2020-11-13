6078ab34365ffaad3108e80d30ec2719c302d8ad
Joseph Herrera <tony.herrera90@gmail.com>
2020-11-13 15:41:55 -0800

    stub boot project with jpa dependency

---
+++ GarrisonHub/src/test/java/com/skilldistillery/garrison/GarrisonHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.garrison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarrisonHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
